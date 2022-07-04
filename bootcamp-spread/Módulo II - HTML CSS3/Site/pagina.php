d54db59fc3ad6c0e5a7023054733f4080cd97a75
anamrcnds <ana.marcondes01@fatec.sp.gov.br>
2022-07-04 14:54:02 -0300

    bootcamp-spread

---
+++ bootcamp-spread/Módulo II - HTML CSS3/Site/pagina.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		  <meta charset="utf-8">
+		  <meta name="viewport" content="width=device-width, initial-scale=1">
+		  <title>Frases de Mad Men</title>
+		  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+		  <link rel="stylesheet" href="/resources/demos/style.css">
+		  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+		  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+		  <script>
+		  $( function() {
+			$( "#accordion" ).accordion();
+		  } );
+		  </script>
+	</head>
+<body>
+ 
+<div id="accordion">
+  <h3>Frases aleatórias</h3>
+  <div>
+		<p>
+			De Mad Men e A Felicidade não se compra.
+		</p>
+  </div>
+  
+  <h3>1</h3>
+  <div>
+		<p>
+		“Eu não acredito em destino. Você faz suas próprias oportunidades.” 
+		</p>
+  </div>
+  
+  <h3>2</h3>
+  <div>
+		<p>
+		"Às vezes quando as pessoas conseguem o que querem, 
+		elas percebem o quanto seus sonhos eram limitados."
+		</p>
+  </div>
+  
+  <h3>3</h3>
+  <div>
+    <p>
+		"Quando um homem entra por uma porta ele carrega consigo
+		sua vida inteira"
+    </p>
+  </div>
+  
+  <h3>4</h3>
+  <div>
+		<p>
+			"Lembre-se: nenhum homem é fracassado quando tem amigos!"
+		</p>
+   </div>
+   
+   <h3>5</h3>
+   <div>
+		<p>
+			"O cliente quase nunca sabe o que quer. Essa é a melhor 
+			hora para surpreendê-lo."
+		</p>
+	</div>
+ 
+</body>
+</html>
