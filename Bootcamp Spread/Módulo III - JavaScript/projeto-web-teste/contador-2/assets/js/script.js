d2534ccc4989ccc19064d5a422e6a8a698b48496
anamrcnds <ana.marcondes01@fatec.sp.gov.br>
2022-07-06 00:55:47 -0300

    projeto contador - desafio

--- Bootcamp Spread/Módulo III - JavaScript/projeto-web-teste/contador-2/assets/js/script.js
+++ Bootcamp Spread/Módulo III - JavaScript/projeto-web-teste/contador-2/assets/js/script.js
@@ -1,5 +1,8 @@
 var currentNumberWrapper = document.getElementById("currentNumber")
 var currentNumber = 0
+var btnAdd = document.getElementsByTagName("button")[1]
+var btnSub = document.getElementsByTagName("button")[0]
+
 
 function increment() {
     currentNumber = currentNumber + 1;
@@ -10,3 +13,6 @@ function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
+
+btnAdd.addEventListener('click', increment)
+btnSub.addEventListener('click', decrement)
