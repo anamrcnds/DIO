201f95702ff812650e41842cdc19b3711be5c4d6
anamrcnds <ana.marcondes01@fatec.sp.gov.br>
2022-07-07 15:39:37 -0300

    Validações adicionadas

--- Bootcamp Spread/Módulo III - JavaScript/exercícios/comparar-numeros.js
+++ Bootcamp Spread/Módulo III - JavaScript/exercícios/comparar-numeros.js
@@ -1,18 +1,27 @@
-let num1, num2, soma;
-let comparaDez, comparaVinte, iguais;
+let valida1, valida2, valida3;
 
 function comparaNumeros(num1, num2){
+    valida1 = validaN1(num1);
+    valida2 = validaN2(num2);
+    valida3 = validaNulo(num1, num2);
 
-    soma = num1 + num2;
+    if(valida1 && valida2 && valida3){
+        let soma = num1 + num2;
 
-    iguais = num1 === num2 ? 'são' : 'não são';
-    comparaDez = soma >= 10 ? 'maior ou igual' : 'menor ou igual';
-    comparaVinte = soma <= 20 ? 'menor ou igual' : 'maior ou igual';
-
-    console.log(`Os números ${num1} e ${num2} ${iguais} iguais. Sua soma é ${soma}, que é ${comparaDez} à 10 e ${comparaVinte} à 20.`)
+        console.log(`Os números ${num1} e ${num2} ${saoIguais(num1, num2)}são iguais. Sua soma é ${soma}, que é ${comparaDez(soma)} 10 e ${comparaVinte(soma)} 20.`);
+    }else{
+        console.log("Insira valores válidos!");
+    }
 }
 
-comparaNumeros(15, 7)
+let validaN1 = n1 => !isNaN(n1);
+let validaN2 = n2 => !isNaN(n2);
+let validaNulo = (n1, n2) => !((n1 === 0) && (n2 === 0));
+let saoIguais = (n1, n2) => iguais = n1 === n2 ? '' : 'não ';
+let comparaDez = soma => soma >= 10 ? 'maior ou igual' : 'menor que';
+let comparaVinte = soma => soma <= 20 ? 'menor ou igual' : 'maior que';
+
+comparaNumeros(10, 10);
 
 
 
