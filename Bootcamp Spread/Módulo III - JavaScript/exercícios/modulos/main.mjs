3605b8d3442d5a19633b25744c7f58b449acfad5
anamrcnds <ana.marcondes01@fatec.sp.gov.br>
2022-07-11 19:46:50 -0300

    Exemplo módulos

---
+++ Bootcamp Spread/Módulo III - JavaScript/exercícios/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Ana", 27));
+console.log(mostraCidade('Ana', 'Sorocaba'));
+console.log(mostraHobby("Ana", "corrida de rua"))
