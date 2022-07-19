c45ef33d9b39ebf561c163c94108d658e2b30ce8
anamrcnds <ana.marcondes01@fatec.sp.gov.br>
2022-07-19 11:40:14 -0300

    Confguração webpack

---
+++ Bootcamp Spread/Módulo IV - React/webpack/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+const App = () => {
+    return (
+        <div>
+            <p>Testando...</p>
+            <Button />
+        </div>
+    )
+}
+
+export default App
